e16df8de4c553735a82a18332215e1cc1b32555e
Dirk Breuer <dirk.breuer@gmail.com>
2014-05-05 16:17:15 +0200

    Added generator for guacamole.yml

---
+++ lib/rails/generators/guacamole/config/config_generator.rb
@@ -0,0 +1,25 @@
+# -*- encoding : utf-8 -*-
+
+require 'rails/generators/guacamole_generator'
+
+module Guacamole
+  module Generators
+    class ConfigGenerator < Rails::Generators::Base
+      desc 'Creates a Guacamole configuration file at config/guacamole.yml'
+
+      argument :database_name, type: :string, optional: true
+
+      def self.source_root
+        @_guacamole_source_root ||= File.expand_path('../templates', __FILE__)
+      end
+
+      def app_name
+        Rails::Application.subclasses.first.parent.to_s.underscore
+      end
+
+      def create_config_file
+        template 'guacamole.yml', File.join('config', 'guacamole.yml')
+      end
+    end
+  end
+end
