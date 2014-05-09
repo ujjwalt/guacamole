1aa35ec1547539c4db3529178f4fa90f7e6f4778
Dirk Breuer <dirk.breuer@gmail.com>
2014-05-09 23:20:03 +0200

    AQLNotSupportedError: Added link to documentation

--- lib/guacamole/exceptions.rb
+++ lib/guacamole/exceptions.rb
@@ -3,7 +3,7 @@
 module Guacamole
   class GenericError < StandardError; end
   class AQLNotSupportedError < GenericError
-    def initialize(msg = 'AQL is an experimental feature. Please activate it in the config.')
+    def initialize(msg = 'AQL is an experimental feature. Please activate it in the config: https://github.com/triAGENS/guacamole#experimental-aql-support')
       super
     end
   end
