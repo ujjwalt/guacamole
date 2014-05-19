bc2472356585216d57a88b40fd6b3ba032972c93
moonglum <moonglum@moonbeamlabs.com>
2014-05-19 12:26:32 -0700

    Remove the debugger gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,12 +2,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
-begin
-  require 'debugger'
-rescue LoadError
-  puts "Debugger is not available. Maybe you're Travis."
-end
-
 RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
